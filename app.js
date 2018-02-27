c6e327cac52e57cd05ce83af4a7296483aa8ea84
ashishcodes4 <ashishcodes4@gmail.com>
2018-02-28 04:33:20 +0530

    We Are GAME, FOLKSgit add .

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ function test () {
 	console.log('this is test function');
 }
 
+function removeTransition(e) {
+	if(e.propertyName !=='transform') return;
+	e.target.classList.remove('playing');
+}
 
 function playSound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 	if(!audio) return ;
 	audio.currentTime =0;
+	key.classList.add('playing');
 	audio.play();
-	console.log('audio played');
 }
 
-document.addEventListener('keydown', );
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
