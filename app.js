110fb432499cc32e055996e74e1569f55ba80bc8
ashishcodes4 <ashishcodes4@gmail.com>
2018-02-28 04:20:00 +0530

    corrected the playsound function.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 // const audio = document.querySelector('key')
 console.log('from script file')
 
-document.addEventListener(`audio[ data-key="${e.key}" ] `)
+// document.addEventListener(`audio[ data-key="${e.key}" ] `)
 
 
 
 
-
+function test () {
+	console.log('this is test function');
+}
 
 
 function playSound(e) {
-	const audio = document.querySelector(` audio[ data-key= " ${e.keyCode} " ] `);
-	const key = document.querySelector('div[data-key="${e.keyCode}"]');
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if(!audio) return ;
 	audio.currentTime =0;
 	audio.play();
+	console.log('audio played');
 }
+
+document.addEventListener('keydown', );
